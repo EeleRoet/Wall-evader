ea1cf4b73af60ec2b3f3c931c399c8aa666d08ba
EeleRoet <eele.roet@hotmail.nl>
2020-03-12 21:31:01 +0100

    prototype

---
+++ Wall-evader/Assets/MoveWall.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveWall : MonoBehaviour
+{
+    private Rigidbody wallBody;
+    [SerializeField] private float speed = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        wallBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        wallBody.velocity = Vector3.back * speed;
+    }
+}
---
+++ Wall-evader/Assets/SpawnWalls.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnWalls : MonoBehaviour
+{
+    [SerializeField] private GameObject wallPrefab;
+    [SerializeField] private Transform spawnPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(wallPrefab, spawnPosition.position, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ( Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(wallPrefab, spawnPosition.position, Quaternion.identity);
+        }
+    }
+}
