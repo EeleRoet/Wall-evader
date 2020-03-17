e972155691a47dc3640021db1d717038640fb135
EeleRoet <eele.roet@hotmail.nl>
2020-03-17 21:24:22 +0100

    new unity version, added spotlights

--- Wall-evader/Assets/MoveWall.cs
+++ Wall-evader/Assets/MoveWall.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class MoveWall : MonoBehaviour
 {
     private Rigidbody wallBody;
-    [SerializeField] private float speed = 1.0f;
+    [SerializeField] private float speed = 10f;
 
     // Start is called before the first frame update
     void Start()
