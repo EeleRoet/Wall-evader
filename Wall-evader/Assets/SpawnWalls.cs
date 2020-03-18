741de109592dc65abe017a6e0a7830105595b99c
EeleRoet <eele.roet@hotmail.nl>
2020-03-18 21:34:41 +0100

    point lights en materials

--- Wall-evader/Assets/SpawnWalls.cs
+++ Wall-evader/Assets/SpawnWalls.cs
@@ -10,7 +10,8 @@ public class SpawnWalls : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(wallPrefab, spawnPosition.position, Quaternion.identity);
+        GameObject newWall = Instantiate(wallPrefab, spawnPosition.position, Quaternion.identity);
+        GetFirstChildren(newWall.transform)[0].Rotate(Vector3.back * 30);
     }
 
     // Update is called once per frame
@@ -20,5 +21,24 @@ public class SpawnWalls : MonoBehaviour
         {
             Instantiate(wallPrefab, spawnPosition.position, Quaternion.identity);
         }
+    }
+
+
+    //method from stackoverflow answer at: 
+    //https://answers.unity.com/questions/496958/how-can-i-get-only-the-childrens-of-a-gameonbject.html
+    private Transform[] GetFirstChildren(Transform parent)
+    {
+        Transform[] children = parent.GetComponentsInChildren<Transform>();
+        Transform[] firstChildren = new Transform[parent.childCount];
+        int index = 0;
+        foreach (Transform child in children)
+        {
+            if (child.parent == parent)
+            {
+                firstChildren[index] = child;
+                index++;
+            }
+        }
+        return firstChildren;
     }
 }
