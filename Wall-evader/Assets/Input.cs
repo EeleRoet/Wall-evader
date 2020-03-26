1da98985db21b7c2be23064b96471ede70c633f5
Max Pera <max.pera@hva.nl>
2020-03-26 13:22:03 +0100

    Player input

---
+++ Wall-evader/Assets/Input.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerInput : MonoBehaviour
+{
+
+    Rigidbody rigidbody;
+    Transform transform;
+    
+    int verhoging = 0;
+    int x_change = 1;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.rotation = new Quaternion(0,0,-90,0);
+        transform.position = new Vector3(-0.6f, 0.5f, 2f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = new Quaternion(x_change,0,-90,0);
+        transform.position = new Vector3(-0.6f, 0.5f+verhoging, 2f);
+    }
+
+    private void Reset()
+    {
+       
+    }
+}
