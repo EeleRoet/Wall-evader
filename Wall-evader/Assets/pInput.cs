7b3ab0f06722eae72acc83da7fb57161d6a85252
Max Pera <max.pera@hva.nl>
2020-03-31 13:17:32 +0200

    ?

---
+++ Wall-evader/Assets/pInput.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class pInput : MonoBehaviour
+{
+
+    public Rigidbody rbody;
+    public Transform target;
+    public Slider slide;
+    Vector3 position;
+
+    int verhoging = 0;
+    int rotate;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        target = GetComponent<Transform>();
+        rbody = GetComponent<Rigidbody>();
+        
+
+
+
+        target.position = new Vector3(-0.6f, 0.5f, 2f);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.eulerAngles
+            transform.Rotate((new Vector3(0, 0, rotate)), Space.Self);
+            target.position = new Vector3(-0.6f, 0.5f + verhoging, 2f);
+        
+
+
+        rotate = (int)Mathf.Cos(slide.value);
+    }
+
+    private void Reset()
+    {
+
+    }
+}
