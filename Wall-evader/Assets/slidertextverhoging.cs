96d9217049344525a3142359413d4b675b0391e4
Max Pera <max.pera@hva.nl>
2020-04-01 12:44:47 +0200

    Laatste tweaks slider

---
+++ Wall-evader/Assets/slidertextverhoging.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class slidertextverhoging : MonoBehaviour
+{
+    Text sliderText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sliderText = GetComponent<Text>();
+
+    }
+
+    // Update is called once per frame
+    public void textUpdate(float value)
+    {
+        sliderText.text = value.ToString();
+    }
+}
