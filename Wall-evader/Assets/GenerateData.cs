90389cf59f461b17b0d70485bebdd539f5c9c80a
joost2000 <joostottens@hotmail.nl>
2020-04-01 14:16:03 +0200

    Revert "tabel update"
    This reverts commit b1613191e1a415fafd449c6b36d56f50dcb50766.

--- Wall-evader/Assets/GenerateData.cs
+++ Wall-evader/Assets/GenerateData.cs
@@ -8,7 +8,13 @@ public class GenerateData : MonoBehaviour
     Text text;
     Text[] data;
 
-    public float randomY, hellingsGetal, startgetal, y_waarde1, y_waarde2, y_waarde3;
+    public float randomY, hellingsGetal, startgetal;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
     public void GenerateNewData()
     {
@@ -21,31 +27,20 @@ public class GenerateData : MonoBehaviour
             hellingsGetal *= -1;
         }
 
-        y_waarde1 = startgetal;
-        y_waarde2 = (float)System.Math.Round(hellingsGetal * 3 + startgetal ,1);
-        y_waarde3 = (float)System.Math.Round(hellingsGetal * 4 + startgetal ,1);
-
         FillTabel();
     }
 
     private void FillTabel()
     {
-        data = GetComponentsInChildren<Text>();
-        for (int i = 0; i < data.Length; i++)
-        {
-            if (data[i].tag == "Y_waarde1")
-            {
-                data[i].text = y_waarde1.ToString();
-            }
-            if (data[i].tag == "Y_waarde2")
-            {
-                data[i].text = y_waarde2.ToString();
-            }
-            if (data[i].tag == "Y_waarde3")
-            {
-                data[i].text = y_waarde3.ToString();
-            }
+        data = GetComponentsInChildren<Text>();       
 
+        foreach (Text item in data)
+        {
+            if (item.tag == "Y_waarde")
+            {
+                item.text = randomY.ToString();
+                randomY += hellingsGetal;
+            }
         }
     }
 }
