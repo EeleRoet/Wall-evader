6f559550f6da0f0226aef1810664cf8141b73d83
EeleRoet <eele.roet@hotmail.nl>
2020-04-10 20:57:52 +0200

    obstruction fade

--- Wall-evader/Assets/viewObstructionScript.cs
+++ Wall-evader/Assets/viewObstructionScript.cs
@@ -3,16 +3,50 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class viewObstructionScript : MonoBehaviour
-{ 
+{
+    private bool fadeObstruction;
+    private bool obstructionActivated;
+    [SerializeField] private Material material;
+    private Color color;
+
+    public void start()
+    {
+        activateObstruction();
+        fadeObstruction = false;
+        obstructionActivated = false;
+    }
+
+    public void Update()
+    {
+        if(!obstructionActivated )
+        {
+            activateObstruction();
+            obstructionActivated = true;
+        }
+
+        if ( fadeObstruction)
+        {
+             color = material.color;
+             color.a -= 0.01f;
+             material.color = color;
+             if(color.a <= 0)
+             {
+                 Debug.Log("cheff");
+                 fadeObstruction = false;
+             }
+        }
+        
+    }
+
     public void deactivateObstruction()
     {
-        gameObject.active = false;
-       
+       fadeObstruction = true;
     }
 
     public void activateObstruction()
     {
-        gameObject.active = true;
+        color = material.color;
+        color.a = 1;
+        material.color = color;
     }
-
 }
