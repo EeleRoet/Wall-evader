42e1a6f203dde97b4abf83c941a53cfbbbed4685
Max Pera <max.pera@hva.nl>
2020-04-24 12:56:49 +0200

    Revert "Player is nu bij de countdown"
    This reverts commit f3a36ac292ab5ce4eadb9e63fa998f02297b4207.

--- Wall-evader/Assets/CountdownTimer.cs
+++ Wall-evader/Assets/CountdownTimer.cs
@@ -45,17 +45,10 @@ public class CountdownTimer : MonoBehaviour
         sizeUpdate();
         colorUpdate();
 
-        if(currentTime >= 0)
-        {
-
-            currentSize = 0;
-
-        }
-
         if(pIn.resetTimer == true)
         {
 
-            
+            CountdownText.enabled = true;
             countdownReset();
 
         }
