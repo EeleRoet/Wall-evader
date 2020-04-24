38734d45fba0bb5f5e6837e3430aa9e0dba976ff
Max Pera <max.pera@hva.nl>
2020-04-24 12:57:00 +0200

    Revert "MergeFix"
    This reverts commit e8481f155fff4a5fa759fcf4f8130f4deb1ccfdd, reversing
    changes made to 5f303e84d9871d223f221657bb13c7693e1c918c.

--- Wall-evader/Assets/ScoreCalcAnimations.cs
+++ Wall-evader/Assets/ScoreCalcAnimations.cs
@@ -91,7 +91,9 @@ public class ScoreCalcAnimations : MonoBehaviour
        
         streakMultiplierText.text = streakMultiplierText.text.Split(' ')[1];
         LeanTween.move(streakMultiplierText.gameObject, baseScoreTextAnimationEnd, addScoreAnimationLength).setEase(LeanTweenType.easeOutQuart).setOnComplete(AddFinalScoreAnimation);
-     
+       //LeanTween.alpha(streakMultiplierText.gameObject, 1f, textFadeTime);
+       // LeanTween.alpha(baseScoreText.gameObject, 0f, textFadeTime);
+
 
     }
 
