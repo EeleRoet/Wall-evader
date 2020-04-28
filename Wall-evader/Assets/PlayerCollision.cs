21efb4c1ff69b11dfde61cbba799d342f1e270d8
Max Pera <max.pera@hva.nl>
2020-04-28 17:02:46 +0200

    Revert "explosion"
    This reverts commit 90a839f2fc1726e38f02ea277607796f3ac7dedb.

--- Wall-evader/Assets/PlayerCollision.cs
+++ Wall-evader/Assets/PlayerCollision.cs
@@ -4,13 +4,10 @@ using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour
 {
-
-    public Explosion explosion;
     void OnTriggerEnter(Collider other)
     {
         Debug.Log(other.gameObject.name);
         FindObjectOfType<PauseMenu>().GameOver();
-        //explosion.Explode();
     }
 
 }
