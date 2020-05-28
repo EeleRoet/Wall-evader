efeee94c6217d6f94be1803f267580c025e2d142
Max Pera <max.pera@hva.nl>
2020-05-28 11:20:42 +0200

    customization

--- Wall-evader/Assets/CustomMenu.cs
+++ Wall-evader/Assets/CustomMenu.cs
@@ -3,11 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Networking;
 
 public class CustomMenu : MonoBehaviour
 {
 
-    
+    public float UnlockScore = ScoreScript.score;
+    public Button greenButton;
+    public Button blueButton;
+    public Button orangeButton;
+    public Button pinkButton;
+    public Button greenSButton;
+    public Button blueSButton;
+    public Button orangeSButton;
+    public Button pinkSButton;
+    public Button goldButton;
+
+
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
@@ -25,7 +38,70 @@ public class CustomMenu : MonoBehaviour
 
 
 
+        if (UnlockScore >= 20)
+        {
+
+            greenButton.interactable = true;
+
         }
+
+        if (UnlockScore >= 50)
+        {
+
+            blueButton.interactable = true;
+
+        }
+
+        if (UnlockScore >= 100)
+        {
+
+            orangeButton.interactable = true;
+
+        }
+
+        if (UnlockScore >= 200)
+        {
+
+            pinkButton.interactable = true;
+
+        }
+
+        if (UnlockScore >= 500)
+        {
+
+            greenSButton.interactable = true;
+
+        }
+
+        if (UnlockScore >= 1000)
+        {
+
+            blueSButton.interactable = true;
+
+        }
+
+        if (UnlockScore >= 2000)
+        {
+
+            orangeSButton.interactable = true;
+
+        }
+
+        if (UnlockScore >= 5000)
+        {
+
+            pinkSButton.interactable = true;
+
+        }
+
+        if (UnlockScore >= 10000)
+        {
+
+            goldButton.interactable = true;
+
+        }
+
+    }
 
 
 }
